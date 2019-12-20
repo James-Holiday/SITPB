2bc16a984ae86a11b0f9890ba90b1a524b8010af
KaaaPowww <jamesholiday22@gmail.com>
2019-12-20 12:35:46 -0700

    fetch call

--- src/components/reservation.js
+++ src/components/reservation.js
@@ -1,49 +1,134 @@
 import React, { Component } from "react";
 
-export default class Reservation extends Component {
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      date: "",
+      event: "",
+      editMode: false,
+      apiUrl: "http://127.0.0.1:5000/",
+      apiAction: "post"
+    };
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleEventChange = this.handleEventChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value });
+  }
+  handlePhoneChange(event) {
+    this.setState({ phone: event.target.value });
+  }
+  handleDateChange(event) {
+    this.setState({ date: event.target.value });
+  }
+  handleEventChange(event) {
+    this.setState({ event: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert("A Reservation was submitted: " + this.state.name);
+    event.preventDefault();
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      date: this.state.date,
+      event: this.state.event
+    };
+
+    fetch("http://127.0.0.1:5000/reservations/create", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: this.state.name,
+        email: this.state.email,
+        phone: this.state.phone,
+        date: this.state.date,
+        event: this.state.event
+      })
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
+  }
+
   render() {
     return (
-      <div className="reserve">
-        <form>
-          <div>
-            <label className="font">
-              Name:
-              <input className="input" type="text" name="name" />
-            </label>
-          </div>
-          <div>
-            <label className="font">
-              Email:
-              <input className="input" type="text" name="Email" />
-            </label>
-          </div>
-          <div>
-            <label className="font">
-              Phone Number:
-              <input className="input" type="text" name="phone" />
-            </label>
-          </div>
-          <div>
-            <label className="font">
-              Date and Time Requested:
-              <input className="input" type="text" name="date and time" />
-            </label>
-          </div>
-
-          <select className="selector">
-            <option selected value="wedding">
-              Wedding
-            </option>
-            <option value="graduation">Graduation</option>
-            <option value="Birthday Party">Birthday Party</option>
-            <option value="other">Other</option>
-          </select>
-          <input className="Submitor" type="submit" value="Submit" />
-        </form>
-        <div className="Resfooter">
-          <h5 className="h5">~Somewhere in Time Photo Booth Inc.~</h5>
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          <label className="font">
+            Name:
+            <input
+              className="input"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+            />
+          </label>
         </div>
-      </div>
+        <div>
+          <label className="font">
+            Email:
+            <input
+              className="input"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleEmailChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label className="font">
+            Phone Number:
+            <input
+              className="input"
+              type="text"
+              value={this.state.phone}
+              onChange={this.handlePhoneChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label className="font">
+            Date and Time Requested:
+            <input
+              className="input"
+              type="text"
+              value={this.state.date}
+              onChange={this.handleDateChange}
+            />
+          </label>
+        </div>
+
+        <select
+          className="selector"
+          value={this.state.event}
+          onChange={this.handleEventChange}
+        >
+          <option selected value="wedding">
+            Wedding
+          </option>
+          <option value="graduation">Graduation</option>
+          <option value="Birthday Party">Birthday Party</option>
+          <option value="other">Other</option>
+        </select>
+        <input className="Submitor" type="submit" value="Submit" />
+      </form>
     );
   }
 }
+
+export default Reservation;
--- src/components/testbooth.js
+++ src/components/testbooth.js
@@ -52,3 +52,52 @@
 // }
 
 // export default App;
+
+// Reservation tab Draft
+
+// import React, { Component } from "react";
+
+// export default class Reservation extends Component {
+//   render() {
+//     return (
+//       <div className="reserve">
+//         <form>
+//           <div>
+//             <label className="font">
+//               Name:
+//               <input className="input" type="text" name="name" />
+//             </label>
+//           </div>
+//           <div>
+//             <label className="font">
+//               Email:
+//               <input className="input" type="text" name="Email" />
+//             </label>
+//           </div>
+//           <div>
+//             <label className="font">
+//               Phone Number:
+//               <input className="input" type="text" name="phone" />
+//             </label>
+//           </div>
+//           <div>
+//             <label className="font">
+//               Date and Time Requested:
+//               <input className="input" type="text" name="date and time" />
+//             </label>
+//           </div>
+
+//           <select className="selector">
+//             <option selected value="wedding">
+//               Wedding
+//             </option>
+//             <option value="graduation">Graduation</option>
+//             <option value="Birthday Party">Birthday Party</option>
+//             <option value="other">Other</option>
+//           </select>
+//           <input className="Submitor" type="submit" value="Submit" />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
